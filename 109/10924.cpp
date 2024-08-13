09ef12d816c952caabe777f8ad4cb5b7b7763456
JiaCheng <83905233+wind9051@users.noreply.github.com>
2024-08-14 00:02:20 +0800

    Create 10924.cpp

---
+++ 109/10924.cpp
@@ -0,0 +1,37 @@
+//UVA10924 Prime Words
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> N(1055, 1);
+void init()
+{
+    for (int i = 2; i*i <= 1050; i++) 
+    {
+        if (N[i])
+        {
+            for (int j = i+i; j <= 1050; j+=i)
+            {
+                N[j] = 0;
+            }
+        }
+    }
+}
+
+int main ()
+{
+    init();
+    
+    string line;
+    while (getline(cin, line))
+    {
+        int sum = 0;
+        for (auto c : line)
+        {
+            if (isupper(c)) sum += c - 'A' + 27;
+            else sum += c - 'a' + 1;
+        }
+        if (N[sum]) printf("It is a prime word.\n");
+        else printf("It is not a prime word.\n");
+    }
+    return 0;
+}
