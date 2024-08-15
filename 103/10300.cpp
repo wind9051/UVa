ac0355674685322726417cb2ec3c133398aa810f
JiaCheng <83905233+wind9051@users.noreply.github.com>
2024-08-15 20:35:34 +0800

    Update 10300.cpp

--- 103/10300.cpp
+++ 103/10300.cpp
@@ -12,19 +12,19 @@ int main()
 {
 	int t;
 	cin >> t;
-    while (t--) 
-    {
-    	int f;
+	while (t--) 
+	{
+		int f;
 		cin >> f;
 		ll money = 0;
-    	while (f--) 
-    	{
-    		ll a, n, l; //area number level 
-    		cin >> a >> n >> l;
-    		money += a * l;
+		while (f--) 
+		{
+			ll a, n, l; //area number level 
+			cin >> a >> n >> l;
+			money += a * l;
 		}
 		printf("%d\n", money);
 	}
-    return 0;
+	return 0;
 }
 //money = ((a/n)*l)*n = a *l
