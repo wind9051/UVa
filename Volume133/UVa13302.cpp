8360a9cfd360e2b2842353734daa294ca8386e0e
wind9051 <jiacheng900501@gmail.com>
2026-03-09 16:06:50 +0800

    重構 UVa13302.cpp，將 typedef 替換為 using，並調整數據結構以提高可讀性

--- Volume133/UVa13302.cpp
+++ Volume133/UVa13302.cpp
@@ -8,40 +8,36 @@
 
 #include <bits/stdc++.h>
 using namespace std;
+using LL = long long;
 
-typedef long long ll;
-typedef pair<ll, ll> pll;
-
-pll sim_fra(ll a, ll b) {
-    ll g = __gcd(a, b);
+vector<LL> sim_fra(LL a, LL b) {
+    LL g = __gcd(a, b);
     a /= g;
     b /= g;
     return {a, b};
 }
 
-//ax / bx > ay / by
-bool is_less(pll &x, pll &y) {
-    return x.first * y.second > y.first * x.second;
+// x = (a, b), y = (c, d)
+// a/b > c/d ->  ad > cb
+bool is_greater(vector<LL> &x, vector<LL> &y) {
+    return x[0] * y[1] > y[0] * x[1];
 }
 
 int main () {
-    ll L, n, d;
+    LL L, n, d;
     while (cin >> L >> n >> d && L && n && d) {
-        pll ans = {};
+        vector<LL> ans = {LLONG_MAX, 1};
         for (int b = 1; b <= n; b++) {
-            /* a/b > d, b = [1~n]
-               a > bd, a = ml
-            */
-            ll m = (b * d + L-1) / L; 
-            //ll m = (ll)ceil((b * d) / L);
-
+            // a/b > d, b = [1~n]
+            // a > bd, a = ml
             
-            pll tmp;
-            tmp = sim_fra(m * L, b);
-            if (b == 1) ans = tmp;
-            else if (is_less(ans, tmp)) ans = tmp;
+            LL m = (b * d + L-1) / L; 
+            // ll m = (ll)ceil((b * d) / L);
+
+            vector<LL> tmp = sim_fra(m * L, b);
+            if(is_greater(ans, tmp)) ans = tmp;
         }
-        printf("%lld/%lld\n", ans.first, ans.second);
+        printf("%lld/%lld\n", ans[0], ans[1]);
     }
     return 0;
 }
